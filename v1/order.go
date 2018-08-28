c1bfe2111f76dee30a90b07119237670fad9f693
周帅 <571840649@qq.com>
2018-08-28 13:31:11 +0800

    订单order还原

--- v1/order.go
+++ v1/order.go
@@ -1,25 +1,15 @@
 package v1
 
 import (
-<<<<<<< HEAD
-	"errors"
-	"fmt"
-=======
->>>>>>> 23e0c7f70f3818c3dfbde1b504303eaa2bd8d37b
 	"time"
-
 	"github.com/launchain/exchange-api"
 )
 
-<<<<<<< HEAD
 // Email ...
-=======
->>>>>>> 23e0c7f70f3818c3dfbde1b504303eaa2bd8d37b
 type Order struct {
 	uri string
 }
 
-<<<<<<< HEAD
 // Code ...
 type ShopCart struct {
 	ID        string    `json:"_id"`
@@ -32,27 +22,6 @@ type ShopCart struct {
 	ExpiredAt time.Time `json:"expired_at"`
 }
 
-// NewEmail ..
-func NewEmail(c *api.Config) *Email {
-	c.Check()
-	uri := c.URI()
-	return &Email{uri: uri}
-}
-
-// FindCode ...
-func (e *Email) FindCode(email, code string) (*Code, error) {
-	if email == "" || code == "" {
-		return nil, errors.New("参数错误")
-	}
-
-	url := fmt.Sprintf("%s/v1/emailcode/%s/email/%s", e.uri, code, email)
-	c := &Code{}
-	err := api.Get(url, c)
-	if err != nil {
-		return nil, err
-	}
-	return c, nil
-=======
 type ShopCartResponse struct {
 	ID        string    `json:"_id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -83,5 +52,4 @@ func (s *Order) ShopCartList(userid string) (*[]ShopCartResponse, error) {
 	}
 
 	return &(out.Data), nil
->>>>>>> 23e0c7f70f3818c3dfbde1b504303eaa2bd8d37b
 }
