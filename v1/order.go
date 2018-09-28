71f1faf36419cb8e88e2c4977ff49323f0b278b6
周帅 <571840649@qq.com>
2018-09-28 16:20:40 +0800

    modify shopcart api, add order api

--- v1/order.go
+++ v1/order.go
@@ -1,55 +1,32 @@
 package v1
 
 import (
-	"time"
-	"github.com/launchain/exchange-api"
+	"fmt"
+	"gitlab.com/asset-exchange/exchange-api"
 )
 
-// Email ...
+// Order ...
 type Order struct {
 	uri string
 }
 
-// Code ...
-type ShopCart struct {
-	ID        string    `json:"id"`
-	Status    int       `json:"status"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Email     string    `json:"email"`
-	Type      int       `json:"type"`
-	Code      string    `json:"code"`
-	ExpiredAt time.Time `json:"expired_at"`
-}
-
-type ShopCartResponse struct {
-	ID        string    `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Apikey    string    `bson:"apikey" json:"apikey"`
-	AssetId   string    `bson:"assetid" json:"assetid"`
-	UserId    string    `bson:"userid" json:"userid"`
-}
-type ShopCartsResponse struct {
-	Count int                `json:"count"`
-	Data  []ShopCartResponse `json:"data"`
-}
-
-// NewEmail ..
-func NewOrder(c *api.Config) *Order {
-	c.Check()
-	uri := c.URI()
+// NewOrder ...
+func NewOrder(config *api.Config) *Order {
+	uri := config.URI()
 	return &Order{uri: uri}
 }
 
-// FindCode ...
-func (s *Order) ShopCartList(userid string) (*[]ShopCartResponse, error) {
+// GetOneOrder ...
+func (o *Order) GetOneOrder(orderId string) (map[string]interface{}, error) {
+	out := make(map[string]interface{})
+	if orderId == "" {
+		return out, nil
+	}
 
-	out := &ShopCartsResponse{}
-	err := api.Get(s.uri+"/v1/shopcart/list/"+userid, out)
+	url := fmt.Sprintf("%s/v1/order/detail/%s", o.uri, orderId)
+	err := api.Get(url, &out)
 	if err != nil {
 		return nil, err
 	}
-
-	return &(out.Data), nil
+	return out, nil
 }
